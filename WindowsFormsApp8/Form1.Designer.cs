32e0587dbdc7082e39ac7ca643288628d634c92e
nurlan3295 <nurlan3295@gmail.com>
2019-01-14 21:43:03 +0400

    Add project files.

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Deserelize = new System.Windows.Forms.Label();
+            this.Find_Box = new System.Windows.Forms.TextBox();
+            this.Ok = new System.Windows.Forms.Button();
+            this.Ad = new System.Windows.Forms.Label();
+            this.Soyad = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Olke = new System.Windows.Forms.Label();
+            this.Seher = new System.Windows.Forms.Label();
+            this.Telefon = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Cinsi = new System.Windows.Forms.Label();
+            this.Kisi = new System.Windows.Forms.RadioButton();
+            this.Qadin = new System.Windows.Forms.RadioButton();
+            this.Netice = new System.Windows.Forms.Button();
+            this.Ad_Box = new System.Windows.Forms.TextBox();
+            this.Soyad_Box = new System.Windows.Forms.TextBox();
+            this.Ata_Box = new System.Windows.Forms.TextBox();
+            this.Olke_Box = new System.Windows.Forms.TextBox();
+            this.Seher_Box = new System.Windows.Forms.TextBox();
+            this.Telefon_BOx = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // Deserelize
+            // 
+            this.Deserelize.AutoSize = true;
+            this.Deserelize.Location = new System.Drawing.Point(179, 39);
+            this.Deserelize.Name = "Deserelize";
+            this.Deserelize.Size = new System.Drawing.Size(56, 13);
+            this.Deserelize.TabIndex = 0;
+            this.Deserelize.Text = "Deserelize";
+            // 
+            // Find_Box
+            // 
+            this.Find_Box.Location = new System.Drawing.Point(388, 36);
+            this.Find_Box.Name = "Find_Box";
+            this.Find_Box.Size = new System.Drawing.Size(100, 20);
+            this.Find_Box.TabIndex = 1;
+            this.Find_Box.Enter += new System.EventHandler(this.Find_Box_Enter);
+            // 
+            // Ok
+            // 
+            this.Ok.Location = new System.Drawing.Point(646, 39);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(75, 23);
+            this.Ok.TabIndex = 2;
+            this.Ok.Text = "Ok";
+            this.Ok.UseVisualStyleBackColor = true;
+            this.Ok.Click += new System.EventHandler(this.Ok_Click);
+            // 
+            // Ad
+            // 
+            this.Ad.AutoSize = true;
+            this.Ad.Location = new System.Drawing.Point(185, 102);
+            this.Ad.Name = "Ad";
+            this.Ad.Size = new System.Drawing.Size(20, 13);
+            this.Ad.TabIndex = 3;
+            this.Ad.Text = "Ad";
+            // 
+            // Soyad
+            // 
+            this.Soyad.AutoSize = true;
+            this.Soyad.Location = new System.Drawing.Point(182, 130);
+            this.Soyad.Name = "Soyad";
+            this.Soyad.Size = new System.Drawing.Size(37, 13);
+            this.Soyad.TabIndex = 4;
+            this.Soyad.Text = "Soyad";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ata.Adi";
+            // 
+            // Olke
+            // 
+            this.Olke.AutoSize = true;
+            this.Olke.Location = new System.Drawing.Point(188, 213);
+            this.Olke.Name = "Olke";
+            this.Olke.Size = new System.Drawing.Size(29, 13);
+            this.Olke.TabIndex = 6;
+            this.Olke.Text = "Olke";
+            // 
+            // Seher
+            // 
+            this.Seher.AutoSize = true;
+            this.Seher.Location = new System.Drawing.Point(188, 248);
+            this.Seher.Name = "Seher";
+            this.Seher.Size = new System.Drawing.Size(35, 13);
+            this.Seher.TabIndex = 7;
+            this.Seher.Text = "Seher";
+            // 
+            // Telefon
+            // 
+            this.Telefon.AutoSize = true;
+            this.Telefon.Location = new System.Drawing.Point(185, 281);
+            this.Telefon.Name = "Telefon";
+            this.Telefon.Size = new System.Drawing.Size(43, 13);
+            this.Telefon.TabIndex = 8;
+            this.Telefon.Text = "Telefon";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Dogum.Tarixi";
+            // 
+            // Cinsi
+            // 
+            this.Cinsi.AutoSize = true;
+            this.Cinsi.Location = new System.Drawing.Point(185, 376);
+            this.Cinsi.Name = "Cinsi";
+            this.Cinsi.Size = new System.Drawing.Size(29, 13);
+            this.Cinsi.TabIndex = 10;
+            this.Cinsi.Text = "Cinsi";
+            // 
+            // Kisi
+            // 
+            this.Kisi.AutoSize = true;
+            this.Kisi.Location = new System.Drawing.Point(313, 376);
+            this.Kisi.Name = "Kisi";
+            this.Kisi.Size = new System.Drawing.Size(41, 17);
+            this.Kisi.TabIndex = 11;
+            this.Kisi.TabStop = true;
+            this.Kisi.Text = "Kisi";
+            this.Kisi.UseVisualStyleBackColor = true;
+            this.Kisi.CheckedChanged += new System.EventHandler(this.Kisi_CheckedChanged);
+            // 
+            // Qadin
+            // 
+            this.Qadin.AutoSize = true;
+            this.Qadin.Location = new System.Drawing.Point(432, 376);
+            this.Qadin.Name = "Qadin";
+            this.Qadin.Size = new System.Drawing.Size(53, 17);
+            this.Qadin.TabIndex = 12;
+            this.Qadin.TabStop = true;
+            this.Qadin.Text = "Qadin";
+            this.Qadin.UseVisualStyleBackColor = true;
+            // 
+            // Netice
+            // 
+            this.Netice.Location = new System.Drawing.Point(191, 415);
+            this.Netice.Name = "Netice";
+            this.Netice.Size = new System.Drawing.Size(317, 23);
+            this.Netice.TabIndex = 13;
+            this.Netice.Text = "Netice";
+            this.Netice.UseVisualStyleBackColor = true;
+            this.Netice.Click += new System.EventHandler(this.Netice_Click);
+            // 
+            // Ad_Box
+            // 
+            this.Ad_Box.Location = new System.Drawing.Point(388, 102);
+            this.Ad_Box.Name = "Ad_Box";
+            this.Ad_Box.Size = new System.Drawing.Size(100, 20);
+            this.Ad_Box.TabIndex = 14;
+            this.Ad_Box.TextChanged += new System.EventHandler(this.asd);
+            // 
+            // Soyad_Box
+            // 
+            this.Soyad_Box.Location = new System.Drawing.Point(388, 123);
+            this.Soyad_Box.Name = "Soyad_Box";
+            this.Soyad_Box.Size = new System.Drawing.Size(100, 20);
+            this.Soyad_Box.TabIndex = 14;
+            // 
+            // Ata_Box
+            // 
+            this.Ata_Box.Location = new System.Drawing.Point(388, 154);
+            this.Ata_Box.Name = "Ata_Box";
+            this.Ata_Box.Size = new System.Drawing.Size(100, 20);
+            this.Ata_Box.TabIndex = 14;
+            // 
+            // Olke_Box
+            // 
+            this.Olke_Box.Location = new System.Drawing.Point(385, 213);
+            this.Olke_Box.Name = "Olke_Box";
+            this.Olke_Box.Size = new System.Drawing.Size(100, 20);
+            this.Olke_Box.TabIndex = 14;
+            // 
+            // Seher_Box
+            // 
+            this.Seher_Box.Location = new System.Drawing.Point(385, 248);
+            this.Seher_Box.Name = "Seher_Box";
+            this.Seher_Box.Size = new System.Drawing.Size(100, 20);
+            this.Seher_Box.TabIndex = 14;
+            // 
+            // Telefon_BOx
+            // 
+            this.Telefon_BOx.Location = new System.Drawing.Point(385, 278);
+            this.Telefon_BOx.Name = "Telefon_BOx";
+            this.Telefon_BOx.Size = new System.Drawing.Size(100, 20);
+            this.Telefon_BOx.TabIndex = 14;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(408, 330);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 15;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Telefon_BOx);
+            this.Controls.Add(this.Seher_Box);
+            this.Controls.Add(this.Olke_Box);
+            this.Controls.Add(this.Ata_Box);
+            this.Controls.Add(this.Soyad_Box);
+            this.Controls.Add(this.Ad_Box);
+            this.Controls.Add(this.Netice);
+            this.Controls.Add(this.Qadin);
+            this.Controls.Add(this.Kisi);
+            this.Controls.Add(this.Cinsi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Telefon);
+            this.Controls.Add(this.Seher);
+            this.Controls.Add(this.Olke);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Soyad);
+            this.Controls.Add(this.Ad);
+            this.Controls.Add(this.Ok);
+            this.Controls.Add(this.Find_Box);
+            this.Controls.Add(this.Deserelize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Deserelize;
+        private System.Windows.Forms.TextBox Find_Box;
+        private System.Windows.Forms.Button Ok;
+        private System.Windows.Forms.Label Ad;
+        private System.Windows.Forms.Label Soyad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Olke;
+        private System.Windows.Forms.Label Seher;
+        private System.Windows.Forms.Label Telefon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Cinsi;
+        private System.Windows.Forms.RadioButton Kisi;
+        private System.Windows.Forms.RadioButton Qadin;
+        private System.Windows.Forms.Button Netice;
+        private System.Windows.Forms.TextBox Ad_Box;
+        private System.Windows.Forms.TextBox Soyad_Box;
+        private System.Windows.Forms.TextBox Ata_Box;
+        private System.Windows.Forms.TextBox Olke_Box;
+        private System.Windows.Forms.TextBox Seher_Box;
+        private System.Windows.Forms.TextBox Telefon_BOx;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,148 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void Find_Box_Enter(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+
+
+        private void Netice_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+            User user = new User()
+            {
+                Ad = Ad_Box.Text,
+                Soyad = Soyad_Box.Text,
+                AtaAdi = Ata_Box.Text,
+                Olke = Olke_Box.Text,
+                Seher = Seher_Box.Text,
+                Telefon = Telefon_BOx.Text,
+                
+               
+            
+            
+        };
+
+
+            string json = JsonConvert.SerializeObject(user);
+            System.IO.File.WriteAllText($"{Ad_Box.Text}.json", json);
+
+            Ad_Box.Text = string.Empty;
+            Soyad_Box.Text = string.Empty;
+            Ata_Box.Text = string.Empty;
+            Olke_Box.Text = string.Empty;
+            Seher_Box.Text = string.Empty;
+            Telefon_BOx.Text = string.Empty;
+            dateTimePicker1.Text = string.Empty;
+            Kisi.Checked = false;
+            Qadin.Checked = false;
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ok_Click(object sender, EventArgs e)
+        {
+
+            Find_Box.Text = string.Empty;
+            if (File.Exists($"{Find_Box.Text}.json"))
+            {
+            string str = File.ReadAllText($"{Find_Box.Text}.json");
+
+                var obj = JsonConvert.DeserializeObject<User>(str);
+             
+                Ad_Box.Text = obj.Ad;
+                Soyad_Box.Text = obj.Soyad;
+                Ata_Box.Text = obj.AtaAdi;
+                Olke_Box.Text = obj.Olke;
+                Seher_Box.Text = obj.Seher;
+                Telefon_BOx.Text = obj.Telefon;
+                Cinsi.Text = obj.Cins;
+
+            }
+
+         
+
+
+
+
+           
+        }
+
+        private void asd(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Kisi_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class User
+    {
+
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string AtaAdi { get; set; }
+        public string Olke { get; set; }
+        public string Seher { get; set; }
+        public string Telefon { get; set; }
+        //private string Time;   { get; set; }
+        public string Cins { get; set; }
+
+
+
+    }
+}
